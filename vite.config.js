d7b5b78b0d3e16c0dc1498372638a22fe99b5139
Liel Katifa <liel.k2005@gmail.com>
2025-06-17 09:25:02 +0300

    chore: add outDir for Vite build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // מוסיף את התיקיה שה-Render מצפה למצוא
+  },
 })
